37a49a3e54a30d49e9cd3d6a1365a5992bc193f4
Mark Reale <mark@bnotions.com>
2013-06-29 12:01:56 -0400

    Header content correction

--- header.php
+++ header.php
@@ -7,21 +7,7 @@
 
         <title><?php bloginfo('name'); ?></title>
         
-        <meta name="keywords" content="Technology Education, Technology, Education, Training, Library, Toronto, HTML, CSS,  JavaScript, PHP, Python, Android, Social Design">
-        <meta name="description" content="This is a learning resource built at the YMC for participants of the YMC and anybody else who finds it useful.">
-        <meta property="og:title" content="BNOTIONS">
-        <meta property="og:url" content="http://bnotions.com">
-        <meta property="og:image" content="http://bnotions.com/assets/BNOTIONS_logo.png">
-        <meta property="og:description" content="Technology and Innovation">
-        <meta property="og:type" content="company">
-        <meta property="fb:admins" content="172004950,676480219">
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:title" content="BNOTIONS">
-        <meta name="twitter:url" content="http://bnotions.com">
-        <meta name="twitter:site" content="@bnotions">
-        <meta name="twitter:image:src" content="http://bnotions.com/assets/BNOTIONS_logo.png">
-        <meta name="twitter:description" content="Technology and Innovation">
-
+        
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
     </head>
 
